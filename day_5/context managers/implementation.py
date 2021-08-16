c3873bfb7b5f58bf2c84bcc68e8539019b963a52
Sovushka <sovushka.sever@gmail.com>
2021-08-16 08:48:27 +0300

    Fixed error

--- day_5/context managers/implementation.py
+++ day_5/context managers/implementation.py
@@ -5,14 +5,15 @@ from contextlib import contextmanager
 def open_file(file_name):
     """Менеджер контекста, по умолчанию печатает количество строк в файле."""
     f = open(file_name, 'r')
-
     try:
         count = sum(1 for _ in f)
-        print('Количество строк в файле:', count + 1)
+        print(f'Количество строк в файле:{count + 1}')
+        yield f
 
     finally:
         f.close()
 
 
 if __name__ == '__main__':
-    open_file('log.txt')
+    with open_file('log.txt') as f:
+        print('Файл открыт для чтения')
