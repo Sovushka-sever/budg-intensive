2a74117e73557e8fed1089e56b8b44a9a9f55279
Sovushka <sovushka.sever@gmail.com>
2021-08-22 21:45:28 +0300

    Answers to the questions of the attempt to solve the tasks

--- day_10/middlewares.py
+++ day_10/middlewares.py
@@ -1,14 +1,9 @@
 import time
-
-from django.contrib.auth import login, logout
 from django.utils.deprecation import MiddlewareMixin
 
 
 class FakeUser:
     # определите у пользователя аттрибуты auth
-
-    # def __init__(self, user):
-    #     self.auth = user.auth
     pass
 
 
@@ -16,64 +11,6 @@ class FakeUser:
 # Помните про __call__()
 class MyMiddleware(MiddlewareMixin):
 
-    # def process_request(self, request):
-    #     self.begin_timestamp = time.time()
-    #     print(f"Время до выполнения запроса: {self.begin_timestamp}")
-    #     time.sleep(1)
-    #     user = FakeUser()
-    #     if request.auth == 'VALID_TOKEN':
-    #         user.auth = True
-    #         request.auth = True
-    #     else:
-    #         user.auth = False
-    #         request.auth = False
-    #
-    #
-    #     return self.get_response(request)
-    #
-    # def process_response(self, request, response):
-    #     end_timestamp = time.time()
-    #     runtime = end_timestamp - self.begin_timestamp
-    #     request.runtime = runtime
-    #     print(f"Время после выполнения запроса: {end_timestamp}")
-    #     print(f"Время выполнения запроса: {request.runtime}")
-    #     return response
-
-    # def __call__(self, request):
-    #
-    #     response = None
-    #     if hasattr(self, 'process_request'):
-    #
-    #         response = self.process_request(request)
-    #     response = response or self.get_response(request)
-    #     if hasattr(self, 'process_response'):
-    #
-    #         response = self.process_response(request, response)
-    #     return response
-
-        # request.runtime = None
-        #
-        # begin_timestamp = time.time()
-        # print(f"Время до выполнения запроса: {begin_timestamp}")
-        # time.sleep(1)
-        #
-        # response = self.get_response(request)
-        #
-        # end_timestamp = time.time()
-        # request.runtime = end_timestamp - begin_timestamp
-        # print(f"Время после выполнения запроса: {end_timestamp}")
-        # print(f"Время выполнения запроса: {request.runtime}")
-
-
-        # user = FakeUser()
-        # if request.auth == 'VALID_TOKEN':
-        #     user.auth = True
-        #     request.auth = True
-        # else:
-        #     user.auth = False
-        #     request.auth = False
-        #
-        # return response
 
     def __call__(self, request):
         response = None
